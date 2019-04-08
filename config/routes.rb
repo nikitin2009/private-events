3554c71785aeab6f80700b6f2b1835df966f0bb9
Alex <nikitin2009@gmail.com>
2019-04-08 14:51:56 +0300

    Add navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'events#index'
   get 'signin', to: "sessions#new"
   post 'signin', to: "sessions#create"
   delete 'signout', to: "sessions#destroy"
