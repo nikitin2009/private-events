275e63e912975d4d83bdd1058d13832cbe13f323
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-04-08 13:33:57 +0300

    Add many-to-many relationship among user and event as an attandance

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  has_many :attendances, foreign_key: "attended_event_id"
   belongs_to :creator, class_name: "User"
+  has_many :attendees, through: :attendances, source: :attendee
 end
 
---
+++ db/migrate/20190408101154_create_attendances.rb
@@ -0,0 +1,11 @@
+class CreateAttendances < ActiveRecord::Migration[5.2]
+  def change
+    create_table :attendances do |t|
+      t.references :attendee
+      t.references :attended_event
+
+      t.timestamps
+    end
+    add_index :attendances, [:attendee_id, :attended_event_id], unique: true
+  end
+end
